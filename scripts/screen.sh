d7c8220eae427d35192949a07585c1c608e4d3f0
Jahongir <Jahamarsi@gmail.com>
2025-04-23 06:39:29 +0500

    0.0.7

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-
 SCREENSHOT_DIR=~/Pictures/screens
 mkdir -p "$SCREENSHOT_DIR"
-
 FILENAME="screenshot-$(date +'%Y-%m-%d-%H%M%S').png"
 FILEPATH="$SCREENSHOT_DIR/$FILENAME"
 
-grim -g "$(slurp)" "$FILEPATH"
-wl-copy < "$FILEPATH"
+pre_size=0
+if [ -f "$FILEPATH" ]; then
+    pre_size=$(stat -c%s "$FILEPATH")
+fi
 
-echo "$FILEPATH" | cliphist encode
-notify-send "Screenshot taken"
+grim -g "$(slurp)" "$FILEPATH" 2>/dev/null
+
+if [ -f "$FILEPATH" ]; then
+    post_size=$(stat -c%s "$FILEPATH")
+    
+    if [ $post_size -gt 0 ] && [ $post_size -ne $pre_size ]; then
+        wl-copy < "$FILEPATH"
+        echo "$FILEPATH" | cliphist encode
+        
+        notify-send "Скриншот сохранен" "Путь: $FILEPATH" -i "$FILEPATH"
+    else
+        rm -f "$FILEPATH"
+    fi
+fi
