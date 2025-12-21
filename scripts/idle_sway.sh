c288f74712cdf6a8eb4db6fd4cdaa176dd3d90c0
Jahongir <Jahamarsi@gmail.com>
2025-12-21 21:17:25 +0500

    1.1

--- scripts/idle_sway.sh
+++ scripts/idle_sway.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 
-exec_always swayidle -w \
-    timeout 600 'swaylock -f -i ~/wayland/walls/gruvwall.png' \
+swayidle -w \
+    timeout 590 'notify-send "lockscreen" "10 секунд" -u critical -t 9000' \
+    timeout 600 'swaylock -f -i ~/wayland/walls/nasa.png' \
     timeout 615 'swaymsg "output * dpms off"' \
         resume 'swaymsg "output * dpms on"' \
     timeout 620 'systemctl suspend' \
-    before-sleep 'swaylock -f -i ~/wayland/walls/gruvwall.png'
+    before-sleep 'swaylock -f -i ~/wayland/walls/nasa.png'
